2d403e8bce3cd8964707802d67784f5391d71e55
Petr Bena <benapetr@gmail.com>
2019-05-28 15:58:39 +0200

    changed license

--- pemath.h
+++ pemath.h
@@ -1,10 +1,14 @@
-// PixelEngine
-// --------------------------------------------------------------------------
-// Copyright (C) Petr Bena - All Rights Reserved
-// Unauthorized copying of this file, via any medium is strictly prohibited
-// Proprietary and confidential
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Lesser General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
 
-// Written by Petr Bena 2019
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Lesser General Public License for more details.
+
+// Copyright (c) Petr Bena 2019
 
 #ifndef PEMATH_H
 #define PEMATH_H
